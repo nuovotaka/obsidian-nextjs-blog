bde256e375345e5ce6fc38f88daa09d7252c59ae
tak n <tkn.infomail@gmail.com>
2023-01-13 15:24:51 +0900

    Change import font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        atkinson: ["Atkinson Hyperlegible"],
+      }
+    },
   },
   plugins: [],
 }
