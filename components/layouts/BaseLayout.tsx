8eec7a1456b4449234da5c67d07baec35284a547
tak n <tkn.infomail@gmail.com>
2023-01-13 13:58:26 +0900

    Change mobile icon

--- components/layouts/BaseLayout.tsx
+++ components/layouts/BaseLayout.tsx
@@ -1,18 +1,18 @@
 import Nav from "components/Nav"
 import { PropsWithChildren } from "react"
-import { GiNotebook, GiOpenBook } from "react-icons/gi"
+import { TbNotebook } from "react-icons/tb"
+import { BsBook } from "react-icons/bs"
 import { FaSchool } from "react-icons/fa"
-import { BsLink45Deg } from "react-icons/bs"
-import { SiAboutdotme } from "react-icons/si"
+import { BsLink45Deg, BsPersonFill } from "react-icons/bs"
 import { MdOutlinePrivacyTip } from "react-icons/md"
 
 const BaseLayout: React.FC<PropsWithChildren> = ({ children }) => {
     const navLinks = [
-      { label: 'Writing', href: '/posts', icon: <GiNotebook /> },
-      { label: 'Library', href: '/books', icon: <GiOpenBook /> },
+      { label: 'Writing', href: '/posts', icon: <TbNotebook /> },
+      { label: 'Library', href: '/books', icon: <BsBook /> },
       { label: 'School', href: '/schools', icon: <FaSchool /> },
       { label: 'Blog', href: 'https://blog.nuovotaka.com', icon: <BsLink45Deg />},
-      { label: 'About', href: '/about', icon: <SiAboutdotme />},
+      { label: 'About', href: '/about', icon: <BsPersonFill />},
       { label: 'Privacy', href: '/privacy', icon: <MdOutlinePrivacyTip />},
     ]
     return (<>
